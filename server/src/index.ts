d149dcbf109e579e5f613422077ba38e8a8e6c62
Milan <milans4122k@gmail.com>
2026-01-15 00:21:48 +0530

    backend complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
@@ -18,16 +18,24 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import { authMiddleware } from "./middleware/authMiddleware";
+import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 // Routes
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
-
+app.use("/applications",applicationRoutes)
+app.use("/properties",propertyRoutes)
+app.use("/leases",leaseRoutes)
 app.use("/tenants",authMiddleware(["tenant"]),tenantRoutes)
+app.use("/manager",authMiddleware(["manager"]),managerRoutes)
 
 // Server
 const PORT = process.env.PORT || 3001;
